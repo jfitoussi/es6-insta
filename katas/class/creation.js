8c4b7cc4c34316cb109f3286e1de00ab4a3a7410
KevinLeBrazidec <lebrazideck@hotmail.fr>
2017-03-28 15:54:36 +0200

    Class creation

--- katas/class/creation.js
+++ katas/class/creation.js
@@ -4,21 +4,22 @@
 describe('class creation', () => {
 
   it('is as simple as `class XXX {}`', function() {
-    let TestClass;
+    class TestClass{}
     
     const instance = new TestClass();
     assert.equal(typeof instance, 'object');
   });
 
   it('class is block scoped', () => {
-    class Inside {}
-    {class Inside {}}
+    {class Inside{}}
     assert.equal(typeof Inside, 'undefined');
   });
   
   it('special method is `constructor`', function() {
     class User {
-      constructor(id) {}
+      constructor(id) {
+        this.id = id;
+      }
     }
     
     const user = new User(42);
@@ -27,6 +28,7 @@ describe('class creation', () => {
 
   it('defining a method is simple', function() {
     class User {
+      writesTests() { return false;}
     }
     
     const notATester = new User();
@@ -36,7 +38,7 @@ describe('class creation', () => {
   it('multiple methods need no commas (opposed to object notation)', function() {
     class User {
       wroteATest() { this.everWroteATest = true; }
-      isLazy() {  }
+      isLazy() {return !this.everWroteATest;}
     }
     
     const tester = new User();
@@ -46,7 +48,7 @@ describe('class creation', () => {
   });
 
   it('anonymous class', () => {
-    const classType = typeof {};
+    const classType = typeof () => {};
     assert.equal(classType, 'function');
   });
 
