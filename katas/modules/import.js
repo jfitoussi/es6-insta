7f6f80f0c79a79dcb562267cb3ecd22e33cf6ac0
clementisaia <coldwave0@live.fr>
2017-03-28 16:07:28 +0200

    12e exercice correction

--- katas/modules/import.js
+++ katas/modules/import.js
@@ -1,7 +1,7 @@
 // 61: modules - import
 // To do: make all tests pass, leave the assert lines unchanged!
 
-import assert from 'assert'; // is only here for completeness, `assert` is always imported by default
+import assert from 'assert';
 import { equal, deepEqual, notEqual } from "assert";
 import { equal as myEqual, default as myAssert } from "assert";
 describe('use `import` to import functions that have been exported (somewhere else)', function() {
