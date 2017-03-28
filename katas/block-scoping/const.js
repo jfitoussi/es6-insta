9bef2b4a039d0d61e4bcb6ea61cba66a413c1eeb
Maxime Signoret <maxime.signoret@gmail.com>
2017-03-28 11:04:30 +0200

    [feat] Block scope

--- katas/block-scoping/const.js
+++ katas/block-scoping/const.js
@@ -3,42 +3,39 @@
 
 describe('`const` is like `let` plus read-only', () => {
 
-  describe('scalar values are read-only', () => {
+    describe('scalar values are read-only', () => {
 
-    it('number', () => {
-      const constNum = 0;
-      constNum = 1;
-      assert.equal(constNum, 0);
+        it('number', () => {
+            const constNum = 0;
+            assert.equal(constNum, 0);
+        });
+
+        it('string', () => {
+            const constString = 'I am a const';
+            assert.equal(constString, 'I am a const');
+        });
+
     });
 
-    it('string', () => {
-      const constString = 'I am a const';
-      constString = 'Cant change you?';
-      assert.equal(constString, 'I am a const');
+    const notChangeable = 23;
+
+    it('const scope leaks too', () => {
+        assert.equal(notChangeable, 23);
     });
 
-  });
-  
-  const notChangeable = 23;
+    describe('complex types are NOT fully read-only', () => {
 
-  it('const scope leaks too', () => {
-    assert.equal(notChangeable, 23);
-  });
-  
-  describe('complex types are NOT fully read-only', () => {
+        it('array', () => {
+            const arr = [42, 23];
+            assert.equal(arr[0], 42);
+        });
 
-    it('array', () => {
-      const arr = [42, 23];
-      arr[0] = 0;
-      assert.equal(arr[0], 42);
+        it('object', () => {
+            const obj = {x: 1};
+            obj.x = 3;
+            assert.equal(obj.x, 3);
+        });
+
     });
-    
-    it('object', () => {
-      const obj = {x: 1};
-      obj.x = 2;
-      assert.equal(obj.x, 3);
-    });
-    
-  });
 
 });
