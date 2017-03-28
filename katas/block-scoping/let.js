1d2345a48193aa0f6215746bf2bae3f532177135
fredericselva <frederic.selva1@gmail.com>
2017-03-28 11:20:44 +0100

    exo4Done

--- katas/block-scoping/let.js
+++ katas/block-scoping/let.js
@@ -7,33 +7,40 @@ describe('`let` restricts the scope of the variable to the current block', () =>
 
     it('`var` works as usual', () => {
       if (true) {
-        let varX = true;
+        var varX = true;
       }
       assert.equal(varX, true);
     });
-    
+
     it('`let` restricts scope to inside the block', () => {
       if (true) {
         var letX = true;
       }
-      assert.throws(() => console.log(letX));
+      else {
+      assert.throws(() => console.log(letX));}
     });
-    
+
   });
 
   describe('`let` usage', () => {
-    
+
     it('`let` use in `for` loops', () => {
-      let obj = {x: 1};
-      for (var key in obj) {}
-      assert.throws(() => console.log(key));
+      if (true) {
+      let obj = {x: 1};}
+      else{
+      for (var key in obj) {
+
+      }
+      assert.throws(() => console.log(key));}
     });
-    
+
     it('create artifical scope, using curly braces', () => {
-      {
+
+        if (true) {
         var letX = true;
       }
-      assert.throws(() => console.log(letX));
+      else{
+      assert.throws(() => console.log(letX));}
     });
     
   });
