94057a25a8538c903b37d8a9cb1f7786e8d09a8a
clementisaia <coldwave0@live.fr>
2017-03-28 16:20:05 +0200

    13e exercice

--- katas/default-parameters/basics.js
+++ katas/default-parameters/basics.js
@@ -4,43 +4,42 @@
 describe('default parameters make function parameters more flexible', () => {
 
   it('define it using an assignment to the parameter `function(param=1){}`', function() {
-    let number = (int) => int;
-    
+    let number = (int = 0 ) => int;
+
     assert.equal(number(), 0);
   });
 
   it('it is used when undefined is passed', function() {
     let number = (int = 23) => int;
-    const param = 42;
-    
+    const param = undefined;
+
     assert.equal(number(param), 23);
   });
 
   it('it is not used when a value is given', function() {
-    function xhr() {
-      return method;  
+    function xhr(method = 'GET') {
+      return method;
     }
-    
     assert.equal(xhr('POST'), 'POST');
   });
 
   it('it is evaluated at run time', function() {
     let defaultValue;
     function xhr(method = `value: ${defaultValue}`) {
-      return method;  
+      return method;
     }
-    
+    defaultValue = 42;
     assert.equal(xhr(), 'value: 42');
     defaultValue = 23;
   });
 
   it('it can also be a function', function() {
-    let defaultValue;
+    let defaultValue = function(){};
     function fn(value = defaultValue()) {
-      return value;  
+      return value;
     }
-    
+
     assert.equal(fn(), defaultValue());
   });
-  
+
 });
