d3483b5d9493986715058a8c83751019b1ca5595
islembagga <islem.bagga@gmail.com>
2017-03-28 11:18:05 +0200

    let declaration
    let declaration

--- katas/block-scoping/let.js
+++ katas/block-scoping/let.js
@@ -3,39 +3,39 @@
 
 describe('`let` restricts the scope of the variable to the current block', () => {
 
-  describe('`let` vs. `var`', () => {
+    describe('`let` vs. `var`', () => {
 
-    it('`var` works as usual', () => {
-      if (true) {
-        let varX = true;
-      }
-      assert.equal(varX, true);
-    });
-    
-    it('`let` restricts scope to inside the block', () => {
-      if (true) {
-        var letX = true;
-      }
-      assert.throws(() => console.log(letX));
-    });
-    
-  });
+        it('`var` works as usual', () => {
+            if (true) {
+                var varX = true;
+            }
+            assert.equal(varX, true);
+        });
 
-  describe('`let` usage', () => {
-    
-    it('`let` use in `for` loops', () => {
-      let obj = {x: 1};
-      for (var key in obj) {}
-      assert.throws(() => console.log(key));
+        it('`let` restricts scope to inside the block', () => {
+            if (true) {
+                var letX = true;
+            }
+            assert.throws(() => console.log(X));
+        });
+
     });
-    
-    it('create artifical scope, using curly braces', () => {
-      {
-        var letX = true;
-      }
-      assert.throws(() => console.log(letX));
+
+    describe('`let` usage', () => {
+
+        it('`let` use in `for` loops', () => {
+            let obj = {x: 1};
+            for (let key in obj) {}
+            assert.throws(() => console.log(key));
+        });
+
+        it('create artifical scope, using curly braces', () => {
+            {
+                let letX = true;
+            }
+            assert.throws(() => console.log(letX));
+        });
+
     });
-    
-  });
-  
+
 });
