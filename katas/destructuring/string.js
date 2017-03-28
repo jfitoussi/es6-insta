461ee349b543400696e43acc77ddfdf25a0a5002
Trochez <juan.trochez95@gmail.com>
2017-03-28 16:57:20 +0200

    [destructuring] string

--- katas/destructuring/string.js
+++ katas/destructuring/string.js
@@ -1,22 +1,32 @@
-// 11: destructuring - string
+// 13: destructuring - defaults
 // To do: make all tests pass, leave the assert lines unchanged!
 
-describe('destructuring also works on strings', () => {
+describe('destructuring can also have default values', () => {
 
-  
-  it('destructure every character', () => {
-    let [a, b, c] = 'abc';
-    assert.deepEqual([a, b, c], ['a', 'b', 'c']);
+  it('for an empty array', () => {
+    const [a:1] = [1];
+    assert.equal(a, 1);
   });
-  
-  it('missing characters are undefined', () => {
-    const [,a, c] = 'ab';
-    assert.equal(c, void 0);
+
+  it('for a missing value', () => {
+    const [,b=2] = [1,,3];
+    assert.equal(b, 2);
   });
-  
-  it('unicode character work too', () => {
-    const [, space, coffee] = 'a ☕';
-    assert.equal(coffee, '\u{2615}');
+
+  it('in an object', () => {
+    const [a, b=2] = [{a: 1}];
+    assert.equal(b, 2);
   });
-  
+
+  it('if the value is undefined', () => {
+    const {a, b=2} = {a: 1, b: void 0};
+    assert.strictEqual(b, 2);
+  });
+
+  it('also a string works with defaults', () => {
+    const [a,b=2] = '1';
+    assert.equal(a, '1');
+    assert.equal(b, 2);
+  });
+
 });
