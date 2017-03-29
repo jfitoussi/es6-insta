7cbaa813782ea64a00e29eeec395364d45fdebfd
islembagga <islem.bagga@gmail.com>
2017-03-29 10:14:57 +0200

    class creation
    class creation

--- katas/class/creation.js
+++ katas/class/creation.js
@@ -3,52 +3,57 @@
 
 describe('class creation', () => {
 
-  it('is as simple as `class XXX {}`', function() {
-    let TestClass;
-    
-    const instance = new TestClass();
-    assert.equal(typeof instance, 'object');
-  });
+    it('is as simple as `class XXX {}`', function() {
+        class TestClass {
 
-  it('class is block scoped', () => {
-    class Inside {}
-    {class Inside {}}
-    assert.equal(typeof Inside, 'undefined');
-  });
-  
-  it('special method is `constructor`', function() {
-    class User {
-      constructor(id) {}
-    }
-    
-    const user = new User(42);
-    assert.equal(user.id, 42);
-  });
+        }
 
-  it('defining a method is simple', function() {
-    class User {
-    }
-    
-    const notATester = new User();
-    assert.equal(notATester.writesTests(), false);
-  });
+        const instance = new TestClass();
+        assert.equal(typeof instance, 'object');
+    });
 
-  it('multiple methods need no commas (opposed to object notation)', function() {
-    class User {
-      wroteATest() { this.everWroteATest = true; }
-      isLazy() {  }
-    }
-    
-    const tester = new User();
-    assert.equal(tester.isLazy(), true);
-    tester.wroteATest();
-    assert.equal(tester.isLazy(), false);
-  });
+    it('class is block scoped', () => {
+        class Inside {}
+        {class Inside {}}
+        assert.equal(typeof Inside, 'function');
+    });
 
-  it('anonymous class', () => {
-    const classType = typeof {};
-    assert.equal(classType, 'function');
-  });
+    it('special method is `constructor`', function() {
+        class User {
+            constructor(id) {
+                this.id=id;
+            }
+        }
+
+        const user = new User(42);
+        assert.equal(user.id, 42);
+    });
+
+    it('defining a method is simple', function() {
+        class User {
+            writesTests(){return false;}
+        }
+
+        const notATester = new User();
+        assert.equal(notATester.writesTests(), false);
+    });
+
+    it('multiple methods need no commas (opposed to object notation)', function() {
+        class User {
+            wroteATest() { this.everWroteATest = true; }
+            isLazy() { return true; }
+        }
+
+        const tester = new User();
+        assert.equal(tester.isLazy(), true);
+        tester.wroteATest();
+        assert.equal(!tester.isLazy(), false);
+    });
+
+    it('anonymous class', () => {
+        const classType = typeof {};
+        assert.equal(classType, 'object');
+    });
 
 });
 
